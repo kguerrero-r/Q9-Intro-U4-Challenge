b3d0961f1b2109766aa727726830acf015481857
kguerrero-r <135247466+kguerrero-r@users.noreply.github.com>
2023-06-01 10:11:04 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+// Write your code below!
+let h1 = document.querySelector(".header"); 
+let button = document.querySelector (".button");
+ 
+button.onclick=function(){	
+h1.style.color="blue";
+
+};
